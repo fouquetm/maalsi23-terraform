3a7bcab7c4427a7bd519ddf3b8149e68c31f1928
Matthieu Fouquet <mfouquet@sqli.com>
2024-02-12 11:03:30 +0100

    adding application insights

--- main.tf
+++ main.tf
@@ -23,5 +23,26 @@ resource "azurerm_linux_web_app" "webapp01" {
   location            = azurerm_service_plan.plan01.location
   service_plan_id     = azurerm_service_plan.plan01.id
 
-  site_config {}
+  site_config {
+  }
+
+  app_settings = {
+    ApplicationInsights__InstrumentationKey = azurerm_application_insights.insights01.instrumentation_key
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "loganalystics01" {
+  name                = "logs-${var.project_name}-01"
+  location            = azurerm_resource_group.app01.location
+  resource_group_name = azurerm_resource_group.app01.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "insights01" {
+  name                = "insights-${var.project_name}-01"
+  location            = azurerm_log_analytics_workspace.loganalystics01.location
+  resource_group_name = azurerm_log_analytics_workspace.loganalystics01.resource_group_name
+  workspace_id        = azurerm_log_analytics_workspace.loganalystics01.id
+  application_type    = "web"
 }
