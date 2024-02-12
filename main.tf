4fd7c3589cfd6c5356e67e99e9458348130af12f
Matthieu Fouquet <mfouquet@sqli.com>
2024-02-12 11:40:51 +0100

    add key vault to store web app secrets

--- main.tf
+++ main.tf
@@ -28,6 +28,11 @@ resource "azurerm_linux_web_app" "webapp01" {
 
   app_settings = {
     ApplicationInsights__InstrumentationKey = azurerm_application_insights.insights01.instrumentation_key
+    DbConnectionString = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.kv01.name};SecretName=DbConnectionString)"
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 }
 
@@ -45,4 +50,25 @@ resource "azurerm_application_insights" "insights01" {
   resource_group_name = azurerm_log_analytics_workspace.loganalystics01.resource_group_name
   workspace_id        = azurerm_log_analytics_workspace.loganalystics01.id
   application_type    = "web"
+}
+
+resource "azurerm_key_vault" "kv01" {
+  name                        = "kv-${var.project_name}-01"
+  location                    = azurerm_resource_group.app01.location
+  resource_group_name         = azurerm_resource_group.app01.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "webapp01" {
+  key_vault_id = azurerm_key_vault.kv01.id
+  tenant_id    = azurerm_linux_web_app.webapp01.identity[0].tenant_id
+  object_id    = azurerm_linux_web_app.webapp01.identity[0].principal_id
+
+  secret_permissions = [
+    "Get"
+  ]
 }
